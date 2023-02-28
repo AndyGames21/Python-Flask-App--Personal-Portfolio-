cb1a195b248b6608f9a3d9a235b21703aac85644
AndyGames21 <77165906+AndyGames21@users.noreply.github.com>
2023-02-28 19:03:53 +0100

    Feat : Deletd .gitignore,modified python,html,css

--- website/views.py
+++ website/views.py
@@ -2,6 +2,17 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+# Home View Route
+@views.route('/home')
 def home():
     return render_template("home.html")
+
+# About Me View Route
+@views.route('/about')
+def about():
+    return render_template("about_me.html")
+
+# Projects View Route
+@views.route('/projects')
+def projects():
+    return render_template("projects.html")
