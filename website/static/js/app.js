7625509baefdf3c02e54fd1cb3e1647177be4e77
AndyGames21 <77165906+AndyGames21@users.noreply.github.com>
2023-03-19 20:18:28 +0100

    Chore: Fixed Issue With Port Disturbance Main.py

--- website/static/js/app.js
+++ website/static/js/app.js
@@ -7,7 +7,7 @@ export const ContactUs = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm('service_hrg0gtn', 'template_5zq4flf', form.current, 'o7qn5seZuDqUyOPd5')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
